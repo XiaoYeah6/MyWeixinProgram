0f41bcfa4f9e929cb27b298cf6dc7847e6aa21e8
unknown <unknown@example.com>
2020-01-22 19:14:01 +0800

    blog

---
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -0,0 +1,197 @@
+const MAX_WORDS_NUM = 140
+const MAX_IMG_NUM = 9
+const db = wx.cloud.database()
+let userInfo = {}
+
+let content = ''
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    wordsNum: 0,
+    footerBottom: 0,
+    images: [],
+    selectPhoto: true,
+  },
+
+  onInput(event) {
+    let wordsNum = event.detail.value.length
+    if (wordsNum >= MAX_WORDS_NUM) {
+      wordsNum = `最大字数为${MAX_WORDS_NUM}`
+    }
+    this.setData({
+      wordsNum
+    })
+    content = event.detail.value
+  },
+
+  onFocus(event) {
+    this.setData({
+      footerBottom: event.detail.height
+    })
+  },
+
+  onBlur() {
+    this.setData({
+      footerBottom: 0
+    })
+  },
+
+  onChooseImage() {
+    let max = MAX_IMG_NUM - this.data.images.length
+    wx.chooseImage({
+      count: max,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        this.setData({
+          images: this.data.images.concat(res.tempFilePaths)
+        })
+        max = MAX_IMG_NUM - this.data.images.length
+        this.setData({
+          selectPhoto: max <= 0 ? false : true
+        })
+      },
+    })
+  },
+
+  onDelImage(event) {
+    this.data.images.splice(event.target.dataset.index, 1)
+    this.setData({
+      images: this.data.images
+    })
+    if (this.data.images.length == MAX_IMG_NUM - 1) {
+      this.setData({
+        selectPhoto: true,
+      })
+    }
+  },
+
+  onPreviewImage(event) {
+    wx.previewImage({
+      urls: this.data.images,
+      current: event.target.dataset.imgsrc,
+    })
+  },
+
+  send() {
+
+    if(content.trim() === ''){
+      wx.showModal({
+        title: '请输入内容',
+        content: '',
+      })
+      return
+    }
+
+    wx.showLoading({
+      title: '发布中',
+      mask: true
+    })
+    let promiseArr = []
+    let fileIds = []
+    for (let i = 0, len = this.data.images.length; i < len; i++) {
+      let p = new Promise((resolve, reject) => {
+        let item = this.data.images[i]
+        let suffix = /\.\w+$/.exec(item)[0]
+        wx.cloud.uploadFile({
+          cloudPath: 'blog/' + Date.now() + '-' + Math.random() * 10000000 + suffix,
+          filePath: item,
+          success: (res) => {
+            console.log(res.fileID)
+            fileIds = fileIds.concat(res.fileID)
+            resolve()
+          },
+          fail: (res) => {
+            console.error(res)
+            reject()
+          }
+        })
+      })
+      promiseArr.push(p)
+    }
+    Promise.all(promiseArr).then((res)=>{
+      db.collection('blog').add({
+        data: {
+          content,
+          img: fileIds,
+          ...userInfo,
+          createTime: db.serverDate(),
+        }
+      }).then((res)=>{
+        wx.hideLoading()
+        wx.showToast({
+          title: '发布成功',
+        })
+        //返回并刷新
+        wx.navigateBack()
+        const pages = getCurrentPages()
+        const prevPage = pages[pages.length - 2]
+        prevPage.onPullDownRefresh()
+      })
+    }).catch((err)=>{
+      wx.hideLoading()
+      wx.showToast({
+        title: '发布失败',
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    userInfo = options
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -1,22 +1,88 @@
 // pages/blog/blog.js
+let keyword = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    modalShow: false,
+    blogList: [],
   },
 
   onPublish(){
+    wx.getSetting({
+      success: (res) => {
+        if(res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:(res) => {
+              this.onLoginSuccess({
+                detail: res.userInfo
+              })
+            }
+          })
+        }else{
+          this.setData({
+            modalShow: true,
+          })
+        }
+      }
+    })
+  },
 
+  onLoginSuccess(event){
+    const detail = event.detail
+    wx.navigateTo({
+      url: `../blog-edit/blog-edit?nickName=${detail.nickName}&avatarUrl=${detail.avatarUrl}`,
+    })
+  },
+  onLoginFail(){
+    wx.showModal({
+      title: '需要用户授权才能发布',
+      content: '',
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this._loadBlogList()
+  },
 
+  onSearch(event){
+    keyword = event.detail.keyword
+    this.setData({
+      blogList: [],
+    })
+    this._loadBlogList(0)
+  },
+
+  _loadBlogList(start = 0){
+    wx.showLoading({
+      title: '拼命加载中',
+    })
+    wx.cloud.callFunction({
+      name: 'blog',
+      data: {
+        keyword,
+        start,
+        count: 10,
+        $url: 'list',
+      }
+    }).then((res) => {
+      this.setData({
+        blogList: this.data.blogList.concat(res.result)
+      })
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  goComment(event){
+    wx.navigateTo({
+      url: '../../pages/blog-comment/blog-comment?blogId=' + event.target.dataset.blogid,
+    })
   },
 
   /**
@@ -51,14 +117,17 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.setData({
+      blogList: []
+    })
+    this._loadBlogList(0)
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this._loadBlogList(this.data.blogList.length)
   },
 
   /**
